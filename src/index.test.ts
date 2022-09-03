724bc5bafeab892ab570008cc19382f8d8ba149d
Jeremy Kahn <jeremyckahn@gmail.com>
2022-09-03 12:52:11 -0500

    feat: implement funAnimalName

---
+++ src/index.test.ts
@@ -0,0 +1,18 @@
+import { funAnimalName } from '../src';
+
+describe('funAnimalName', () => {
+  test('creates a fun animal name', () => {
+    const name = funAnimalName('123');
+
+    expect(name).toEqual('Glamorous Starfish');
+  });
+
+  test('creates a fun animal name based on provided configuration', () => {
+    const name = funAnimalName('123', {
+      animalNames: ['Cow'],
+      adjectives: ['Happy'],
+    });
+
+    expect(name).toEqual('Happy Cow');
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+import { animalNames as defaultAnimalNames } from './data/animal-names';
+import { adjectives as defaultAdjectives } from './data/adjectives';
+
+const convertStringToInteger = (str: string) => {
+  return str
+    .split('')
+    .reduce((acc, char, i) => acc + char.charCodeAt(0) * i, 0);
+};
+
+export interface FunAnimalNameConfig {
+  animalNames?: string[];
+  adjectives?: string[];
+}
+
+export const funAnimalName = (
+  stringToHash: string,
+  {
+    adjectives = defaultAdjectives,
+    animalNames = defaultAnimalNames,
+  }: FunAnimalNameConfig = {}
+): `${string} ${string}` => {
+  const hashNumber = convertStringToInteger(stringToHash);
+  const adjective = adjectives[hashNumber % adjectives.length];
+  const adjectiveNumberValue = convertStringToInteger(adjective);
+
+  const animal =
+    animalNames[(hashNumber + adjectiveNumberValue) % animalNames.length];
+
+  return `${adjective} ${animal}`;
 };
